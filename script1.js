1cffbf3de0d064a31ecad4e4ee8f8ca087fd8732
Taubek-29 <taubek.smagulov27@fizmat.kz>
2025-04-05 14:10:32 +0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -54,7 +54,6 @@ document.addEventListener('DOMContentLoaded', function() {
             'theme-minimal',
             'theme-creative',
             'theme-tech',
-            'theme-elegant'
         );
         
         // Добавляем выбранную тему
